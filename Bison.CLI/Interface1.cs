803866dff9b0909112c4137ec67c52f38598d774
Santiag007 <johbe@itu.dk>
2026-09-02 10:48:17 +0200

    lavet simpel interface uden indhold, Co authored by Jens jenss@itu.dk

--- Bison.CLI/Interface1.cs
+++ Bison.CLI/Interface1.cs
@@ -1,6 +1,15 @@
 ﻿namespace Bison.CLI;
+using System.Globalization;
 
 public interface Interface1
 {
+    static void PrintObservations(IEnumerable<T> cheeps)
+    {
+        foreach (var cheep in cheeps)
+        {
+            var time = DateTimeOffset.FromUnixTimeSeconds(long.Parse(cheep.TimeStamp));
+            Console.WriteLine($"{cheep.Author} @ {time.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture)}: {cheep.Message}");
+        }
+    }
 
 }
