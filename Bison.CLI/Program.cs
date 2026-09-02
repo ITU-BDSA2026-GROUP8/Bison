7ebc74e1d9b39123f1266977902424a993e8ce10
Jens Martin Bennekou <jmbe@itu.dk>
2026-09-02 11:40:28 +0200

    !Refactor: Added CsvHelper to program Refactored program.cs to use CsvHelper instead of parsing it manualy Authored-by Jens jmbe@itu.dk

--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -1,9 +1,21 @@
 ﻿using System.Globalization;
+using CsvHelper;
 
+
+public record Cheep(
+    [index(0)]
+    string Author,
+    [index(1)]
+    string Message, 
+    [index(2)]
+    long Timestamp
+    );
+static void Main(string[] args){
+    
 if (args.Length > 0 && args[0] == "read")
 {
 
-    var lines = File.ReadAllLines("bison_observe_cli_db.csv");
+   /* var lines = File.ReadAllLines("bison_observe_cli_db.csv");
     foreach (var line in lines)
     {
         var parts = line.Split(',');
@@ -11,6 +23,16 @@ if (args.Length > 0 && args[0] == "read")
         var message = parts[1].Trim('"');
         var time = DateTimeOffset.FromUnixTimeSeconds(long.Parse(parts[2]));
         Console.WriteLine($"{author} @ {time.ToString ("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture)}: {message}");
+    }*/
+
+    var config = new CsvConfiguration(CultureInfo.InvariantCulture){
+    HasHeaderRecord = false,
+    };
+    using (var reader = new StreamReader("bison_observe_cli_db.csv"))
+    using (var csv = new CsvReader(reader,config))
+    {
+        var records = csv.GetRecords<Cheep>();
+        Console.WriteLine(records);
     }
 }
 
@@ -21,5 +43,6 @@ if (args[0] == "observe" && args.Length >= 2)
     var time = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
     var csvLine = $"{author},\"{message}\",{time}";
     File.AppendAllText("bison_observe_cli_db.csv", csvLine + Environment.NewLine);
+}
 }
 
