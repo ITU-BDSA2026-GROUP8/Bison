994f527ba719b255dc65018340dc1c05d77a410f
Santiag007 <johbe@itu.dk>
2026-09-02 16:24:22 +0200

    moved the printing method from CsvHelper to UserInterface Co Authored by jenss@itu.dk

--- Bison.CLI/Interface1.cs
+++ Bison.CLI/Interface1.cs
@@ -1,15 +1,24 @@
 ﻿namespace Bison.CLI;
 using System.Globalization;
+using CsvHelper.Configuration;
+using CsvHelper;
 
 public interface Interface1
 {
-    static void PrintObservations(IEnumerable<T> cheeps)
+    static void PrintObservations()
     {
-        foreach (var cheep in cheeps)
-        {
-            var time = DateTimeOffset.FromUnixTimeSeconds(long.Parse(cheep.TimeStamp));
-            Console.WriteLine($"{cheep.Author} @ {time.ToString("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture)}: {cheep.Message}");
-        }
+        var config = new CsvConfiguration(CultureInfo.InvariantCulture)
+            {
+                HasHeaderRecord = false,
+            };
+            using (var reader = new StreamReader("bison_observe_cli_db.csv")) using (var csv = new CsvReader(reader, config))
+            {
+                var records = csv.GetRecords<Cheep>();
+                foreach(var record in records)
+                {
+                    var time = DateTimeOffset.FromUnixTimeSeconds(record.Timestamp);
+                    Console.WriteLine($"{record.Author} @ {time.ToString ("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture)}: {record.Message}");
+                }
+            }
     }
-
 }
--- Bison.CLI/Program.cs
+++ Bison.CLI/Program.cs
@@ -8,25 +8,11 @@ using CsvHelper;
 
 public class Program
 {
-    public record Cheep([property: Index(0)] string Author, [property: Index(1)] string Message, [property: Index(2)] long Timestamp);
     static void Main(string[] args)
     {
         if (args.Length > 0 && args[0] == "read")
         {
-
-            var config = new CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = false,
-            };
-            using (var reader = new StreamReader("bison_observe_cli_db.csv")) using (var csv = new CsvReader(reader, config))
-            {
-                var records = csv.GetRecords<Cheep>();
-                foreach(var record in records)
-                {
-                    var time = DateTimeOffset.FromUnixTimeSeconds(record.Timestamp);
-                    Console.WriteLine($"{record.Author} @ {time.ToString ("MM/dd/yy HH:mm:ss", CultureInfo.InvariantCulture)}: {record.Message}");
-                }
-            }
+            Interface1.PrintObservations();
         }
 
         if (args[0] == "observe" && args.Length >= 2)
@@ -39,3 +25,4 @@ public class Program
         }
     }
 }
+public record Cheep([property: Index(0)] string Author, [property: Index(1)] string Message, [property: Index(2)] long Timestamp);
