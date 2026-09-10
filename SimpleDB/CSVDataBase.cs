341cb66fa439d18ceb46c6e3bcf0f51bbf709800
Santiag007 <johbe@itu.dk>
2026-09-10 22:10:47 +0200

    .

--- SimpleDB/CSVDataBase.cs
+++ SimpleDB/CSVDataBase.cs
@@ -8,74 +8,46 @@ using Microsoft.VisualBasic;
 
 public sealed class CSVDataBase<T> : IDatabaseRepository<T>
 {
-    IEnumerable<T> Comments;
-    IEnumerable<T> Observations;
-
+    CsvReader CSVReader;
     CsvConfiguration Config;
 
-    string Peter = "..//SimpleDB//bison_observe_cli_db.csv";
-    string Peter2 = "..//SimpleDB//bison_comment_cli_db.csv";
+    string FilePath = "..//SimpleDB//bison_observe_cli_db.csv";
 
-    CSVDataBase()
+    public CSVDataBase(String identity)
     {
-        this.Config = new CsvConfiguration(CultureInfo.InvariantCulture)
+        this.Config = new CsvConfiguration(CultureInfo.InvariantCulture) { HasHeaderRecord = false, };
+
+        if (identity == "observation")
         {
-            HasHeaderRecord = false,
-        };
-
-        using (var reader = new StreamReader(this.Peter))
-        using (var csv = new CsvReader(reader, this.Config)) this.Observations = csv.GetRecords<T>();
-
-        using (var reader = new StreamReader(this.Peter2))
-        using (var csv = new CsvReader(reader, this.Config)) this.Comments = csv.GetRecords<T>();        
+            this.FilePath = "..//SimpleDB//bison_observe_cli_db.csv";
+            var ObservationReader = new StreamReader(this.FilePath);
+            this.CSVReader = new CsvReader(ObservationReader, this.Config);
+        }
+        else
+        {
+            this.FilePath = "..//SimpleDB//bison_comment_cli_db.csv";
+            var CommentReader = new StreamReader(this.FilePath);
+            this.CSVReader = new CsvReader(CommentReader, this.Config);
+        }
     }
 
     public void Store(T record)
     {
-        using var writer = new StreamWriter(this.Peter, true);
-
+        using var writer = new StreamWriter(this.FilePath, true);
         using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
         {
             csv.WriteRecord(record);
             csv.NextRecord();
         }
-    }
-    public void StoreComment(T record)
-    {
-        using var writer = new StreamWriter(this.Peter2, true);
 
-        using var csv = new CsvWriter(writer, CultureInfo.InvariantCulture);
-        {
-            csv.WriteRecord(record);
-            csv.NextRecord();
-        }
     }
 
     public IEnumerable<T> Read(int? limit = null)
     {
-        using (var reader = new StreamReader(this.Peter))
-        using (var csv = new CsvReader(reader, this.Config))
-        {
-            var records = csv.GetRecords<T>().ToList<T>();
-            return records;
-
-        }
+        return this.CSVReader.GetRecords<T>().ToList();
     }
-    public IEnumerable<T> CommentsRead(int? limit = null)
-    {
-        using (var reader = new StreamReader(this.Peter2))
-        using (var csv = new CsvReader(reader, this.Config))
-        {
-            var records = csv.GetRecords<T>().ToList<T>();
-            return records;
-        }
-    }
-
     public int GetCount()
     {
-        using var reader = new StreamReader(this.Peter);
-        using var csv = new CsvReader(reader, this.Config);
-
-        return csv.GetRecords<T>().Count();
+        return this.CSVReader.GetRecords<T>().Count();
     }
 }
