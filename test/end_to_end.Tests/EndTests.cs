1d11bb67d035e0e5047be7e88516d68a40aaea93
Santiag007 <johbe@itu.dk>
2026-09-20 16:33:48 +0200

    Added test for all possible correct commands

--- test/end_to_end.Tests/EndTests.cs
+++ test/end_to_end.Tests/EndTests.cs
@@ -7,9 +7,33 @@ using Bison.CLI;
 public class EndTests
 {
     [Fact]
-    public async Task EndToEndTest()
+    public async Task ReadTest()
     {
         string[] input = {"read"};
+        var exitCode = await Program.Main(input);
+        Assert.Equal(0, exitCode);
+    }
+
+    [Fact]
+    public async Task ObserveTest()
+    {  
+        string[] input = {"observe", "noget her"};
+        var exitCode = await Program.Main(input);
+        Assert.Equal(0, exitCode);
+    }
+    
+    [Fact]
+    public async Task CommentTest()
+    {
+        string[] input = {"comment", "a new comment", "1"};
+        var exitCode = await Program.Main(input);
+        Assert.Equal(0, exitCode);
+    }
+
+    [Fact]
+    public async Task DiscussionTest()
+    {
+        string[] input = {"discussion", "1"};
         var exitCode = await Program.Main(input);
         Assert.Equal(0, exitCode);
     }
